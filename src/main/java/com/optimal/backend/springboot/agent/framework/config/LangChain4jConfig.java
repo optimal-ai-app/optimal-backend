4f0b4afbcf841330f5fc877ebafb4f7bcfabaa40
Nir <ngub24@gmail.com>
2026-01-14 22:31:38 -0800

    Update LangChain4jConfig.java

--- src/main/java/com/optimal/backend/springboot/agent/framework/config/LangChain4jConfig.java
+++ src/main/java/com/optimal/backend/springboot/agent/framework/config/LangChain4jConfig.java
@@ -1,11 +1,23 @@
 package com.optimal.backend.springboot.agent.framework.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.chat.DisabledChatModel;
+import dev.langchain4j.model.chat.request.ResponseFormat;
+import dev.langchain4j.model.chat.request.ResponseFormatType;
+import dev.langchain4j.model.chat.request.json.JsonArraySchema;
+import dev.langchain4j.model.chat.request.json.JsonBooleanSchema;
+import dev.langchain4j.model.chat.request.json.JsonNumberSchema;
+import dev.langchain4j.model.chat.request.json.JsonObjectSchema;
+import dev.langchain4j.model.chat.request.json.JsonSchema;
+import dev.langchain4j.model.chat.request.json.JsonSchemaElement;
+import dev.langchain4j.model.chat.request.json.JsonStringSchema;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 // import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
 
@@ -33,26 +45,16 @@ public class LangChain4jConfig {
             System.err.println("WARNING: OpenAI API key is not configured. Using disabled chat model.");
             return new DisabledChatModel();
         }
-        // try {
-        // return GoogleAiGeminiChatModel.builder()
-        // .apiKey(geminiApiKey)
-        // .modelName("gemini-2.5-flash")
-        // .temperature(.2)
-        // .logResponses(true)
-        // .returnThinking(true)
-        // .build();
-        // } catch (Exception e) {
-        // System.err.println("ERROR: Failed to create Main chat model: " +
-        // e.getMessage());
-        // return new DisabledChatModel();
-        // }
+
         try {
             return OpenAiChatModel.builder()
                     .apiKey(gptApiKey)
                     .modelName("gpt-4.1")
-                    .temperature(0.1)
+                    .temperature(0.2)
                     .logResponses(true)
                     .strictTools(true)
+                    .strictJsonSchema(true)
+                    .responseFormat(getResponseFormat())
                     .build();
         } catch (Exception e) {
             System.err.println("ERROR: Failed to create Light chat model: " +
@@ -87,5 +89,38 @@ public class LangChain4jConfig {
                     e.getMessage());
             return new DisabledChatModel();
         }
+    }
+
+    private ResponseFormat getResponseFormat() {
+        JsonArraySchema tagsSchema = JsonArraySchema.builder()
+                .items(JsonStringSchema.builder()
+                        .description(
+                                "A raw JSON string containing the arbitrary data object. Do not escape quotes unnecessarily.")
+                        .build())
+                .build();
+
+        JsonSchemaElement dataSchema = JsonStringSchema.builder()
+                .description("A raw JSON string containing dynamic key-value pairs (e.g.,{\"goalId\": 123}).")
+                .build();
+
+        Map<String, JsonSchemaElement> jsonProperties = new HashMap<>();
+
+        jsonProperties.put("content", JsonStringSchema.builder().build());
+        jsonProperties.put("readyToHandoff", JsonBooleanSchema.builder().build());
+        jsonProperties.put("reInterpret", JsonBooleanSchema.builder().build());
+        jsonProperties.put("currentStep", JsonNumberSchema.builder().build());
+        jsonProperties.put("tags", tagsSchema);
+        jsonProperties.put("data", dataSchema);
+
+        return ResponseFormat.builder()
+                .type(ResponseFormatType.JSON)
+                .jsonSchema(JsonSchema.builder()
+                        .name("ChatResponse")
+                        .rootElement(JsonObjectSchema.builder()
+                                .addProperties(jsonProperties)
+                                .required("content", "readyToHandoff", "reInterpret")
+                                .build())
+                        .build())
+                .build();
     }
 }
